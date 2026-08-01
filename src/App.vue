a214efa7b5c18db6affd8a92c631e2b2301d5c33
みゃみゅ玉子 <myamyu@gmail.com>
2026-08-02 02:25:45 +0900

    リポジトリの初期設定 (#1)

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="min-h-screen bg-orange-50 flex flex-col items-center justify-center p-4"
+  >
+    <h1 class="text-3xl font-bold text-orange-600 mb-2">Gaorin Seeker</h1>
+    <p class="text-orange-800">
+      準備完了にゃ！ここからゲームを作っていくにゃ！
+    </p>
+  </div>
+</template>
+
+<script setup></script>
